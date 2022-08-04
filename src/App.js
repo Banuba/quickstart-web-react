be1c91eb3f2fb31265015c625308e7496084fbbc
Artyom Stepanishchev <artem.stepanishchev@banuba.com>
2022-08-04 20:33:57 +0400

    Reformat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
-import { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from "react"
 import { BANUBA_CLIENT_TOKEN } from "./BanubaClientToken"
 import { Webcam, Player, Module, Effect, Dom } from "@banuba/webar"
 
@@ -18,36 +18,32 @@ function App() {
 
   // componentDidMount
   useEffect(() => {
-    const webcam = ref.current.webcam ??= new Webcam()  
-    const promise = ref.current.player ??= Player
-        .create({
-          clientToken: BANUBA_CLIENT_TOKEN,
-          /**
-           * By default BanubaSDK.js tries to loads BanubaSDK.wasm and BanubaSDK.data files relative to itself.
-           * Since the BanubaSDK.js will be bundled to something like `static/js/[name].[hash].js` during a build
-           * and the BanubaSDK.wasm and BanubaSDK.data files may not lay next to the bundle file
-           * we have to tell the BanubaSDK where it can find these BanubaSDK.wasm and BanubaSDK.data files.
-           * @see {@link https://docs.banuba.com/generated/typedoc/globals.html#sdkoptions} further information}
-           */
-          locateFile: {
-            "BanubaSDK.wasm": wasm,
-            "BanubaSDK.simd.wasm": simd,
-            "BanubaSDK.data": data,
-          },
-        })
-        .then((player) => {
-          player.addModule(new Module(FaceTracker))
-            .then(() => {
-              player.use(webcam)
-              player.applyEffect(new Effect("effects/glasses_Banuba.zip"))
-              Dom.render(player, "#webar")
-            })
-        })
+    const webcam = (ref.current.webcam ??= new Webcam())
+    const promise = (ref.current.player ??= Player.create({
+      clientToken: BANUBA_CLIENT_TOKEN,
+      /**
+       * By default BanubaSDK.js tries to loads BanubaSDK.wasm and BanubaSDK.data files relative to itself.
+       * Since the BanubaSDK.js will be bundled to something like `static/js/[name].[hash].js` during a build
+       * and the BanubaSDK.wasm and BanubaSDK.data files may not lay next to the bundle file
+       * we have to tell the BanubaSDK where it can find these BanubaSDK.wasm and BanubaSDK.data files.
+       * @see {@link https://docs.banuba.com/generated/typedoc/globals.html#sdkoptions} further information}
+       */
+      locateFile: {
+        "BanubaSDK.wasm": wasm,
+        "BanubaSDK.simd.wasm": simd,
+        "BanubaSDK.data": data,
+      },
+    }).then((player) =>
+      player.addModule(new Module(FaceTracker)).then(() => {
+        player.use(webcam)
+        player.applyEffect(new Effect("effects/glasses_Banuba.zip"))
+        Dom.render(player, "#webar")
+      })
+    ))
 
     return () => {
       webcam.stop()
       Dom.unmount("#webar")
-      promise.then((player) => player.destroy())
     }
   })
 
@@ -55,21 +51,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div id="webar" style={{ maxWidth: '600px' }}></div>
+        <div id="webar" style={{ maxWidth: "600px" }}></div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
